b4b46cdf0216f39646f1145c00fb5f80e127dea7
Eraiyamuthan P <eraiamuthu57@gmail.com>
2026-02-11 13:33:09 +0530

    Update deepface_backend.py

--- server/deepface_backend.py
+++ server/deepface_backend.py
@@ -14,7 +14,6 @@ CORS(app,
          "origins": [
              "http://localhost:8080",
              "http://localhost:3000", 
-             "https://eraiyamuthan-p.github.io",
              "https://eraiyamuthan-p.github.io/Password-Manager-with-Multi-Factor-Authentication"
          ],
          "methods": ["GET", "POST", "OPTIONS"],
